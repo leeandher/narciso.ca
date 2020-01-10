1377f9928207096e23481baa00aad16491768bc4
Leander Rodrigues <me@leander.xyz>
2020-01-09 22:48:58 -0800

    :wrench: Modify configuration files

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Narciso Rodrigues Real Estate`,
+    description: `Narciso Rodrigues Real Estate Broker - Friend first, realtor second.`,
+    author: `@leeandher`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -27,8 +27,9 @@ module.exports = {
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
+    `gatsby-plugin-styled-components`,
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
+    `gatsby-plugin-offline`,
   ],
 }
