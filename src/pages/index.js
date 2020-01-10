1ea0b92aa58b6ce39df7e8c6270e69ae81049fcd
Leander Rodrigues <me@leander.xyz>
2020-01-09 22:50:32 -0800

    :recycle: Rework styling system

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,26 @@
 import React from "react"
 import { Link } from "gatsby"
 
-import Layout from "../components/layout"
-import Image from "../components/image"
-import SEO from "../components/seo"
+import styled from "styled-components"
+import tw from "tailwind.macro"
 
+import Layout from "../components/Layout"
+import Image from "../components/Image"
+import SEO from "../components/SEO"
+
+const Button = styled.button`
+  ${tw`bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full`};
+`
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
     <h1>Hi people</h1>
     <p>Welcome to your new Gatsby site.</p>
+    <Button>Testing!</Button>
+    <link
+      href="https://fonts.googleapis.com/css?family=Solway&display=swap"
+      rel="stylesheet"
+    />
     <p>Now go build something great.</p>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
---
+++ src/styles/palette.js
@@ -0,0 +1,4 @@
+const palette = {}
+
+export { palette }
+export default palette
